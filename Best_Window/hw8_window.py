ee44df8a2e752bd88351197ff99f49a7f5ed2d3b
jonorgan <jonorgan26@gmail.com>
2023-10-31 18:18:04 -0400

    Finished question 2

--- Best_Window/hw8_window.py
+++ Best_Window/hw8_window.py
@@ -3,8 +3,9 @@
 ###
 
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 from sklearn import utils
 
@@ -71,6 +72,53 @@ def Q1EvaluateW(w_row):
 
 q1_df = pd.DataFrame(np.arange(5, 31), columns=['W'])
 q1_df['Average P/L'] = q1_df.apply(Q1EvaluateW, axis=1)
+
+def Q1GenerateGraph(data, col1, col2, xlab, ylab, _title, q):
+	fig, ax = plt.subplots()
+	ax.plot(data[col1], data[col2])
+	ax.set(xlabel=xlab, ylabel=ylab,
+	       title=_title)
+	ax.grid()
+	print("Saving " + _title + " graph...")
+	fig.savefig("Q" + q + ".png")
+
+Q1GenerateGraph(q1_df, "W", "Average P/L", 'W Value', 'W Average P/L', 'W Average P/L by trade', "1_W_averagePL")
+print("The optimal W* value of W is 8")
+
+
+print("\n")
+# Question 2 ====================================================================================================
+print("Question 2:")
+
+
+def Q2GenerateDF():
+	data = []
+
+	i = 0
+	count = 1
+	model = LinearRegression(fit_intercept=True)
+	while i < file_length:
+		if df['Year'].iloc[i].astype(str) == "2017":
+			i += 1
+			continue
+		if df['Year'].iloc[i].astype(str) == "2019":
+			i = file_length
+			continue
+
+		x = np.arange(1, 9)
+		y = df['Adj Close'][(df.index <= i) & (df.index > i - 8)]
+		x_2 = x[:, np.newaxis]
+		model.fit(x_2, y)
+
+		data.append([count, model.score(x_2, y)])
+		count += 1
+
+		i += 1
+	temp_df = pd.DataFrame(data, columns=['Date_num', 'R2_Val'])
+	return temp_df
+
+q2_df = Q2GenerateDF()
+Q1GenerateGraph(q2_df, "Date_num", "R2_Val", 'Day', 'R Squared Value', 'R Squared value by Day for Y2', "2_R2byDay")
 
 
 
