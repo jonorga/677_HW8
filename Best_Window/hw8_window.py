7d4c88fceb7523b12d6f7069e8eb5220194e42ee
jonorgan <jonorgan26@gmail.com>
2023-10-30 23:38:42 -0400

    Progress on question 1

--- Best_Window/hw8_window.py
+++ Best_Window/hw8_window.py
@@ -18,19 +18,23 @@ print("Question 1:")
 def Q1EvaluateW(w):
 	i = w
 	model = LinearRegression(fit_intercept=True)
+	trade_count = 0
+	trade_sum = 0
 	while i < file_length:
 		if df['Year'].iloc[i].astype(str) == "2018":
 			i = file_length
 
-		if w == i:		
-			#x = df.index[(df.index <= i) & (df.index > i - w)].to_numpy().reshape(-1, 1)
-			x = np.arange(1, w + 1)
-			y = df['Adj Close'][(df.index <= i) & (df.index > i - w)]
-			x_2 = x[:, np.newaxis]
-			model.fit(x_2, y)
-			#print(y)
-			#print(model.predict([[i + 1]]))
-			#print(df['Adj Close'].iloc[i + 1])
+		x = np.arange(1, w + 1)
+		y = df['Adj Close'][(df.index <= i) & (df.index > i - w)]
+		x_2 = x[:, np.newaxis]
+		model.fit(x_2, y)
+		#print(y)
+		#print(model.predict([[i + 1]]))
+		#print(df['Adj Close'].iloc[i + 1])
 		i += 1
+	if trade_count == 0:
+		return -1
+	else:
+		return trade_sum / trade_count
 
 Q1EvaluateW(5)
